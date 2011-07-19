e94e05853c45939846d4978cd735059aaf0f4cc2
Linus Nilsson <linus.nilsson@preventbevakning.se>
2011-07-19 13:20:43 +0200

    Finnished title Helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # Retrun a title on a per-page basis
+  def title
+    base_title = "Rails testing"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+
 end
